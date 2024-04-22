9b73c319a6668a911d78065382dc30a0f8fee91e
AIB1TAL0S <111070767+AIB1TAL0S@users.noreply.github.com>
2024-04-22 22:57:56 +0800

    changes
    admin

--- src/app/admin/manageinstructor/manageinstructor.component.ts
+++ src/app/admin/manageinstructor/manageinstructor.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
-
+import axios from 'axios';
 @Component({
   selector: 'app-sample-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   templateUrl: './manageinstructor.component.html',
   styleUrls: ['./manageinstructor.component.scss']
 })
@@ -19,4 +19,35 @@ export class SysManageInstructorComponent {
   showTableView() {
     this.isTilesView = false;
   }
+  instructors: any[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.fetchInstructors();
+  }
+
+  async fetchInstructors() {
+    try {
+      const response = await axios.get('https://66216a2427fcd16fa6c6e28f.mockapi.io/API/V1/users');
+
+      this.instructors = response.data.map((user) => ({
+        id: user.id,
+        lastName: user.lastname, 
+        firstName: user.firstname, 
+        middleName: user.middlename, 
+        numberOfClass: parseInt(user.numberofclass, 10),
+        rfid: 'RFID' + user.rfid, 
+        mobile: user.mobile, 
+        domainEmail: user.domainemail, 
+        altEmail: user.altemail, 
+        picture: user.picture
+      }));
+
+      console.log('Fetched instructors:', this.instructors);
+    } catch (error) {
+      console.error('Error fetching instructors: ', error);
+    }
+  }
 }
+
