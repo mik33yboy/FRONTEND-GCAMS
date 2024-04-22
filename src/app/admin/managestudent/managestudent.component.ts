906716b8d7e23c7d3e8c6e2d74cf01dda8f6b545
JenisheSalem <202211660@gordoncollege.edu.ph>
2024-04-22 23:32:02 +0800

    sdfgh

--- src/app/admin/managestudent/managestudent.component.ts
+++ src/app/admin/managestudent/managestudent.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
+import axios from 'axios';
+
 
 @Component({
   selector: 'app-sample-page',
@@ -18,5 +20,36 @@ export class SysManageStudentsComponent {
 
   showTableView() {
     this.isTilesView = false;
+  }
+
+  students: any[] = [];
+
+  constructor(){}
+
+  ngOnInit() {
+    this.fetchStudents();
+  }
+
+  async fetchStudents() {
+    try {
+      const response = await axios.get('https://66216a2427fcd16fa6c6e28f.mockapi.io/API/V1/users');
+
+      this.students = response.data.map((user) => ({
+        id: user.id,
+        lastName: user.lastname, 
+        firstName: user.firstname, 
+        middleName: user.middlename, 
+        numberOfClass: parseInt(user.numberofclass, 10),
+        rfid: 'RFID' + user.rfid, 
+        mobile: user.mobile, 
+        domainEmail: user.domainemail, 
+        altEmail: user.altemail, 
+        picture: user.picture
+      }));
+
+      console.log('Fetched instructors:', this.students);
+    } catch (error) {
+      console.error('Error fetching instructors: ', error);
+    }
   }
 }
